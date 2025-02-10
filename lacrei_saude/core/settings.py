69135b3aa0e8c75542bad5152844f38fdead2b70
jess <70455379+jesslourenco@users.noreply.github.com>
2025-02-10 10:00:21 -0300

    added skeleton code , docker setup ok (app + postgres)

--- lacrei_saude/core/settings.py
+++ lacrei_saude/core/settings.py
@@ -76,11 +76,13 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        'ENGINE': "django.db.backends.postgresql",
+        "NAME": "postgresdb",
+        "USER": "postgres",
+        "PASSWORD": "postgres",
+        "HOST": "db",  
+        "PORT": "5432",    }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
