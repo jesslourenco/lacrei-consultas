9575030f22cf312413be77d2c6601d31aaa664b2
jess <70455379+jesslourenco@users.noreply.github.com>
2025-02-15 20:19:41 -0300

    bug fix em PUT consultas (sem validacao) + dockerfile mais simples

--- lacrei_saude/api/services/consulta_service.py
+++ lacrei_saude/api/services/consulta_service.py
@@ -5,6 +5,19 @@ from django.core.exceptions import ObjectDoesNotExist
 from api.repositories.consulta_repo import ConsultaRepo
 from api.models.consulta import Consulta
 
+def validate_appt(profissional_id: int, data_hora):
+    """ Valida se os dia e hora da consulta sao validos (dentro do horario comercial, segunda a sexta)
+        e se o horario desejado ja nao foi agendado.
+    """
+    if ConsultaRepo.get_by_provider_and_datetime(profissional_id, data_hora):
+        raise ValidationError("Horário já está agendado para este profissional.")
+        
+    if data_hora.hour < 8 or data_hora.hour >= 18:
+        raise ValidationError("Consultas devm ser agendadas entre 08:00 e 18:00.")
+    
+    if data_hora.weekday() in [5, 6]: 
+        raise ValidationError("A consulta não pode ser agendada em finais de semana.")
+
 class ConsultaService:
     def list_appts() -> List[Consulta]:
         return ConsultaRepo.get_all()
@@ -19,14 +32,7 @@ class ConsultaService:
         return ConsultaRepo.get_upcoming_by_provider(profissional_id)
 
     def book_appt(paciente_id: int, profissional_id: int, data_hora) -> Consulta:
-        if ConsultaRepo.get_by_provider_and_datetime(profissional_id, data_hora):
-            raise ValidationError("Horário já está agendado para este profissional.")
-        
-        if data_hora.hour < 8 or data_hora.hour >= 18:
-            raise ValidationError("Consultas devm ser agendadas entre 08:00 e 18:00.")
-    
-        if data_hora.weekday() in [5, 6]: 
-            raise ValidationError("A consulta não pode ser agendada em finais de semana.")
+        validate_appt(profissional_id, data_hora)
 
         return ConsultaRepo.create(paciente_id, profissional_id, data_hora)
 
@@ -34,6 +40,9 @@ class ConsultaService:
         appt = ConsultaRepo.get_by_id(pk)
         if not appt:
             raise ObjectDoesNotExist("Consulta nao encontrada.")
+        
+        validate_appt(appt.profissional_id, data["data_hora"])
+        
         return ConsultaRepo.update(appt, data)
 
     def delete_consulta(pk: int) -> None:
